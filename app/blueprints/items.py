2ea3cde58968b43fd905c5fc56321585378b59eb
rygon <nfollis26@gmail.com>
2025-11-26 12:03:43 +0800

    Overhauled, migrated to db

--- app/blueprints/items.py
+++ app/blueprints/items.py
@@ -1,17 +1,12 @@
 import json
 from dataclasses import dataclass
+from itertools import groupby
 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, abort, render_template, request
 from sqlalchemy import select
 
-from app.blueprints.utils import (
-    get_comp,
-    get_status_equip,
-    make_valid_class_name,
-    process_styled_text,
-)
 from app.extensions import db
-from app.models import Item, Shop
+from app.models import Arsenal, Item, ItemCategory, Shop
 
 bp = Blueprint("items", __name__, url_prefix="/items", static_folder="../static/")
 
@@ -42,8 +37,6 @@ class FEUnit:
     name: str
 
 
-ITEMS = {}
-ITEM_CATS = {}
 UNITS = {}
 ARSENAL_UNITS = {}
 with bp.open_resource("../static/json/arsenals.json", "r") as f:
@@ -68,40 +61,6 @@ def init_lists() -> None:
                     ARSENAL_UNITS[unit_cat] = [
                         {"nid": unit_nid, "name": UNITS[unit_nid]["name"]}
                     ]
-    with bp.open_resource("../static/json/items.json", "r") as fp:
-        for data_entry in json.load(fp):
-            if target := [
-                x for x in data_entry["components"] if x[0].startswith("target")
-            ]:
-                target = target[0][0].split("_")[1].title()
-            else:
-                target = ""
-            ITEMS[data_entry["nid"]] = FEItem(
-                nid=data_entry["nid"],
-                name=data_entry["name"],
-                desc=process_styled_text(data_entry["desc"]),
-                value=get_comp(data_entry, "value", int),
-                weapon_rank=get_comp(data_entry, "weapon_rank", str),
-                weapon_type=get_comp(data_entry, "weapon_type", str),
-                damage=get_comp(data_entry, "damage", int),
-                weight=get_comp(data_entry, "weight", int),
-                crit=get_comp(data_entry, "crit", int),
-                hit=get_comp(data_entry, "hit", int),
-                min_range=get_comp(data_entry, "min_range", int),
-                max_range=get_comp(data_entry, "max_range", int),
-                status_on_equip=get_status_equip(data_entry),
-                target=target,
-                icon_class=(
-                    f"{make_valid_class_name(data_entry['nid'])}-item-icon {make_valid_class_name(data_entry['icon_nid'])}-icon"
-                    if data_entry["icon_nid"]
-                    else ""
-                ),
-                sub_items=get_comp(data_entry, "multi_item", list),
-            )
-
-    with bp.open_resource("../static/json/items.category.new.json", "r") as fp:
-        for x, x_val in json.load(fp).items():
-            ITEM_CATS[x] = x_val
 
 
 init_lists()
@@ -109,16 +68,59 @@ init_lists()
 
 @bp.route("/")
 def get_fe_item_index() -> str:
-    if item_nid := request.args.get("itemSelect"):
-        template = "item_sheet.html.jinja2"
-    else:
-        item_nid = "Iron_Sword"
-        template = "item_index.html.jinja2"
-    item_data = db.get_or_404(Item, item_nid)
+    stmt = select(ItemCategory).order_by(ItemCategory.order_key)
+    item_cats = db.session.execute(stmt).scalars().all()
+    ordered_item_cats = groupby(item_cats, key=lambda x: x.type)
+    selected_category = request.args.get("selectedCategory")
     return render_template(
-        template,
-        item_data=item_data,
-        item_cats=ITEM_CATS,
+        "item_index.html.jinja2",
+        item_cats=ordered_item_cats,
+        selected_category=selected_category,
+    )
+
+
+@bp.route("/categories")
+def get_item_list():
+    if not (item_cat_nid := request.args.get("itemCategory")):
+        item_cat_nid = "wtype_Sword"
+    item_cat = db.get_or_404(ItemCategory, item_cat_nid)
+    unordered_items = item_cat.items
+    if not (item_cat_sort := request.args.get("itemSort")):
+        item_cat_sort = "wrank_inc"
+    grouped_items = []
+    sort_reverse = item_cat_sort.endswith("dec")
+    if item_cat_sort.startswith("wrank_"):
+        unordered_items.sort(key=lambda x: x.weapon_rank)
+        for group_name, group_iterator in groupby(
+            unordered_items, key=lambda x: x.weapon_rank
+        ):
+            group_list = list(group_iterator)
+            order_key = int(group_list[0].weapon_rank_order_key)
+            grouped_items.append(
+                {
+                    "key": group_name,
+                    "order": order_key,
+                    "items": group_list,
+                }
+            )
+        grouped_items.sort(key=lambda x: x["order"], reverse=sort_reverse)
+    elif item_cat_sort.startswith("alpha_"):
+        unordered_items.sort(key=lambda x: x.name[0].upper())
+        for group_name, group_iterator in groupby(
+            unordered_items, key=lambda x: x.name[0].upper()
+        ):
+            group_list = list(group_iterator)
+            grouped_items.append(
+                {
+                    "key": group_name,
+                    "items": sorted(group_list, key=lambda x: x.name),
+                }
+            )
+        grouped_items.sort(key=lambda x: x["key"], reverse=sort_reverse)
+
+    return render_template(
+        "item_index_list.jinja2",
+        grouped_items=grouped_items,
     )
 
 
@@ -135,20 +137,26 @@ def get_fe_arsenal() -> str:
     else:
         fe_unit_nid = "Eirika"
         template = "arsenal_index.html.jinja2"
+    stmt = select(Arsenal).where(Arsenal.arsenal_owner_nid == fe_unit_nid)
+    unit_arsenals = db.session.execute(stmt).scalars().all()
+    if not unit_arsenals:
+        abort(404)
     return render_template(
         template,
         unit_data=UNITS[fe_unit_nid],
-        unit_arsenals=ARSENALS[fe_unit_nid],
+        unit_arsenals=unit_arsenals,
         arsenal_units=ARSENAL_UNITS,
     )
 
 
 @bp.route("/arsenals/<string:fe_unit_nid>")
-def get_fe_arsenal_sheet(fe_unit_nid="Artur") -> str:
+def get_fe_arsenal_sheet(fe_unit_nid="Eirika") -> str:
+    stmt = select(Arsenal).where(Arsenal.arsenal_owner_nid == fe_unit_nid)
+    unit_arsenals = db.session.execute(stmt).scalars().all()
     return render_template(
         "arsenal_sheet.html.jinja2",
         unit_data=UNITS[fe_unit_nid],
-        unit_arsenals=ARSENALS[fe_unit_nid],
+        unit_arsenals=unit_arsenals,
         arsenal_units=ARSENAL_UNITS,
     )
 
